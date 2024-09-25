bf76984ffd812a778b0bcc766ff11cd42ae186bf
Varshini3222 <Varshini3222>
2024-09-25 07:12:57 +0530

    first commit

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require("mongoose");
 //const dbURI = "mongodb://localhost/Loc8r";
 const dbURI = "mongodb+srv://22eg106b26:1234@eventcluster.4osji.mongodb.net/";
@@ -36,43 +35,4 @@ process.on("SIGTERM", () => {
     process.exit(0);
   });
 });
-=======
-const mongoose = require("mongoose");
-//const dbURI = "mongodb://localhost/Loc8r";
-const dbURI = "mongodb+srv://22eg106b26:1234@eventcluster.4osji.mongodb.net/";
-mongoose.connect(dbURI, { useNewUrlParser: true });
-mongoose.connection.on("connected", () => {
-  console.log(`Mongoose connected to ${dbURI}`);
-});
-mongoose.connection.on("error", (err) => {
-  console.log(`Mongoose connection error: ${err}`);
-});
-mongoose.connection.on("disconnected", () => {
-  console.log("Mongoose disconnected");
-});
-const gracefulShutdown = (msg, callback) => {
-  mongoose.connection.close(() => {
-    console.log(`Mongoose disconnected through ${msg}`);
-    callback();
-  });
-};
-// For nodemon restarts
-process.once("SIGUSR2", () => {
-  gracefulShutdown("nodemon restart", () => {
-    process.kill(process.pid, "SIGUSR2");
-  });
-});
-// For app termination
-process.on("SIGINT", () => {
-  gracefulShutdown("app termination", () => {
-    process.exit(0);
-  });
-});
-// For Heroku app termination
-process.on("SIGTERM", () => {
-  gracefulShutdown("Heroku app shutdown", () => {
-    process.exit(0);
-  });
-});
->>>>>>> e6c4061f6b4b65a70c97651b0b5914c6f7de8cfc
 require("./locations.js");
--- app_server/routes/auth.js
+++ app_server/routes/auth.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/auth');
@@ -16,22 +15,3 @@ router.get('/login', authController.renderLogin);
 router.post('/login', authController.loginUser);
 
 module.exports = router;
-=======
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/auth');
-
-// Route for signup page
-router.get('/signup', authController.renderSignup);
-
-// Route to handle user signup form submission
-router.post('/signup', authController.registerUser);
-
-// Route for login page
-router.get('/login', authController.renderLogin);
-
-// Route to handle user login form submission
-router.post('/login', authController.loginUser);
-
-module.exports = router;
->>>>>>> e6c4061f6b4b65a70c97651b0b5914c6f7de8cfc
